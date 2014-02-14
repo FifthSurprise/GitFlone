789acbec6cf5d6656dc8ec8359be6924dd504984
Kevin Chang <kevin.w.chang@gmail.com>
2014-02-14 00:34:01 -0500

    After cloning, now opens pull request for branch to main via browser

--- quickFlone.rb
+++ quickFlone.rb
@@ -1,4 +1,3 @@
-
 def fork(user, source,repoName)
   command = "curl -u '#{user}' https://api.github.com/repos/#{source}/#{repoName}/forks -d '{}'"
   puts `#{command}`
@@ -10,11 +9,21 @@ def clone (user,repoName, branch="Feature")
   Dir.chdir("#{repoName}")
   command = "git checkout -b #{branch}"
   puts`#{command}`
+  command = "git push origin #{branch}"
+  puts`#{command}`
+end
+
+def pull ()
+  command = %Q[open https://github.com/#{@user}/#{@repoName}/compare/#{@branch}?expand=1]
+  puts `#{command}`
 end
 
 @user = ARGV[0]
-@gitdirectory = ARGV[1]
+@gitdirectory = ARGV[1].split(":").last
 @branch = ARGV[2].nil? ? "Feature" : ARGV[2]
+@source = @gitdirectory.split("/").first
+@repoName = (@gitdirectory.split("/").last).split(".").first
 
-fork(@user,source,repoName)
-clone(@user,repoName,@branch)
+fork(@user,@source,@repoName)
+clone(@user,@repoName,@branch)
+pull()
