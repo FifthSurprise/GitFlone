5c80affb0f0de9bc9d8a4f681e94da14d0e26f5f
Kevin Chang <kevin.w.chang@gmail.com>
2014-02-13 22:05:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Quick Flone
+
+Fork a repository on github and then clone the forked repository to the current directory.  Afterwards, create a new branch.
+
+Currently, a password must be put in.
+
+Parameters are: Username, SSH Github Repo link, NewBranchName (Defaults to "Feature")
---
+++ quickFlone.rb
@@ -0,0 +1,20 @@
+
+def fork(user, source,repoName)
+  command = "curl -u '#{user}' https://api.github.com/repos/#{source}/#{repoName}/forks -d '{}'"
+  puts `#{command}`
+end
+
+def clone (user,repoName, branch="Solution")
+  command = "git clone git@github.com:#{user}/#{repoName}.git"
+  puts`#{command}`
+  Dir.chdir("#{repoName}")
+  command = "git checkout -b #{branch}"
+  puts`#{command}`
+end
+
+@user = ARGV[0]
+@gitdirectory = ARGV[1]
+@branch = ARGV[2].nil? ? "Solution" : ARGV[2]
+
+fork(@user,source,repoName)
+clone(@user,repoName,@branch)
